bab37dce0264e9b06b7b8a54ba103f1ef197bb17
PETER ANNA RAJU P <petertrichy@gmail.com>
2020-05-05 20:26:17 +0530

    microblog:step5.1 user login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+@login.user_loader(id)
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -28,4 +34,4 @@ class Post(db.Model):
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-        
+
