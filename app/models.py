ef10aaa24d5d118f2a5d80328ca5da436388b895
PETER ANNA RAJU P <petertrichy@gmail.com>
2020-05-05 22:05:27 +0530

    microblog:step5.2 Flask Login completed.

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
-@login.user_loader(id)
+@login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
