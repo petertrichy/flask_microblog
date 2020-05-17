bb8579e2f50b436281c623524c079a904caae5fd
PETER ANNA RAJU P <petertrichy@gmail.com>
2020-05-17 15:48:23 +0530

    microblog:step10 email support done

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, validators
+from wtforms.validators import DataRequired, Email
+from wtforms.validators import ValidationError, EqualTo, Length
 from app.models import User
 
 
@@ -47,4 +48,15 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
 
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password',
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
 
